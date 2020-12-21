defc924a14bf3dadf1a2bc460f5ce6db9d13519d
Joep Hinderink <hello@joephinderink.nl>
2020-12-21 16:23:39 +0100

    Initial Commit

---
+++ MapView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapView
+//
+//  Created by Joep Hinderink on 21/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapView/MapViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewApp.swift
+//  MapView
+//
+//  Created by Joep Hinderink on 21/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct MapViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
