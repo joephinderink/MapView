6665681638c3b3d9bebd93bf9e5737607271bc07
Joep Hinderink <hello@joephinderink.nl>
2021-01-14 20:31:54 +0100

    Update

--- MapView/ContentView.swift
+++ MapView/ContentView.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 52.08, longitude: 4.3 ), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    private let places: [Location] = [
+    Location(coordinate: CLLocationCoordinate2D(latitude: 52.08, longitude: 4.3 ))
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Map(coordinateRegion: $region, annotationItems: places , annotationContent: { place in
+            MapMarker(coordinate: place.coordinate)
+        })
+            .ignoresSafeArea(.all, edges: .vertical)
+            .statusBar(hidden: true)
     }
+}
+
+struct Location: Identifiable {
+    let id = UUID()
+    var coordinate: CLLocationCoordinate2D
 }
 
 struct ContentView_Previews: PreviewProvider {
